cd57bfb992745c05d978d4365212cc94a03a75a4
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-10-22 19:05:30 +0300

    mod

--- src/components/committees job/committee-link.vue
+++ src/components/committees job/committee-link.vue
@@ -1,12 +1,12 @@
 <template>
-  <li class="flex flex-col items-center w-[300px] p-2 border mx-2 bg-blue-600">
-    <img src="https://placehold.co/300x300" class="" />
-    <router-link
-      class="text-center text-white font-semibold tracking-wide hover:tracking-widest transition-all duration-300"
-      :to="`/committee/${i}`"
-      :key="committee"
-    >
-      0{{ index + 1 }} {{ committee.name }}
+  <li class="w-[300px] p-2 rounded-lg mx-2 bg-blue-600">
+    <router-link :to="`/committee/${i}`" :key="committee">
+      <img src="https://placehold.co/300x300" />
+      <p
+        class="text-center text-white font-semibold tracking-wide hover:tracking-widest transition-all duration-300"
+      >
+        0{{ index + 1 }} {{ committee.name }}
+      </p>
     </router-link>
   </li>
 </template>
