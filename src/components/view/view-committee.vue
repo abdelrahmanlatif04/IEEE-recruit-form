461f57f41adace39a5f41c91b98c4dd1d140aca3
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-10-27 00:13:53 +0300

    mod

--- src/components/view/view-committee.vue
+++ src/components/view/view-committee.vue
@@ -1,4 +1,31 @@
-<template>{{ committeeApplicants }}</template>
+<template>
+  <div class="flex flex-col gap-1">
+    <div v-for="i in committeeApplicants" :key="i">
+      <p>
+        Name :
+        {{ i.name }}
+      </p>
+
+      <p>
+        Email :
+        {{ i.email }}
+      </p>
+      <p>
+        University :
+        {{ i.university }}
+      </p>
+      <p>
+        Faculty :
+        {{ i.faculty }}
+      </p>
+
+      <p>
+        Grade :
+        {{ i.year }}
+      </p>
+    </div>
+  </div>
+</template>
 
 <script>
 import axios from "axios";
@@ -20,7 +47,7 @@ export default {
         }
       )
       .then((response) => {
-        this.committeeApplicants = response;
+        this.committeeApplicants = response["data"]["applications"];
       })
       .catch((error) => {
         console.error(error);
