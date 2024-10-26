9d9130a3c3ea24ac44865de72ae51789e3da9a8e
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-10-27 02:20:31 +0300

    mod

--- src/components/committees-job.vue
+++ src/components/committees-job.vue
@@ -3,7 +3,7 @@
     class="flex flex-col gap-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
   >
     <div
-      :class="`shadow flex w-[400px] aspect-video bg-[url('./bg.jpg')] rounded-lg overflow-hidden relative`"
+      :class="`shadow flex w-[400px] aspect-video bg-[url('/bg.jpg')] rounded-lg overflow-hidden relative`"
     >
       <div
         class="flex transition-transform h-full duration-300"
