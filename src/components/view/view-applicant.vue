d6599a0f6e88e924b8898b67ad5333c2a1010af8
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-11-07 07:19:29 +0200

    mod

--- src/components/view/view-applicant.vue
+++ src/components/view/view-applicant.vue
@@ -59,8 +59,7 @@
       >
         Accepted
       </p>
-      <p v-else class="text-2xl text-[#1e40af]">Rejected</p>
-      <template>
+      <template v-else>
         <button
           title="approve"
           class="p-2 w-10 h-10 text-white rounded-full bg-green-500 flex justify-center items-center"
